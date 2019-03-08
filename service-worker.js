f5bffa6391151b271fcde1e48077f05d78c769a1
Alertis <osmanyavuzdemir96@gmail.com>
2019-03-08 23:51:44 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/bookstore/v1/precache-manifest.0fb42c8736269c78f65fd9cb4e05790b.js"
+  "/bookstore/precache-manifest.ce84d5dfe7daa157e00f0ee975e57ab9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/bookstore/v1/index.html", {
+workbox.routing.registerNavigationRoute("/bookstore/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
