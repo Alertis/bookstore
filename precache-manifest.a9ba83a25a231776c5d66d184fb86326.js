07c77261abe30feb90b375fe550abb2a9343cece
Alertis <osmanyavuzdemir96@gmail.com>
2019-03-09 01:46:32 +0300

    Updates

--- precache-manifest.a9ba83a25a231776c5d66d184fb86326.js
+++ precache-manifest.a9ba83a25a231776c5d66d184fb86326.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/bookstore/static/js/runtime~main.c5eccaef.js"
   },
   {
-    "revision": "fb9e131d8e642f3597ae",
-    "url": "/bookstore/static/js/main.fb9e131d.chunk.js"
+    "revision": "5fbdad14d5373972ab42",
+    "url": "/bookstore/static/js/main.5fbdad14.chunk.js"
   },
   {
     "revision": "3101ede5fa18db0d7863",
     "url": "/bookstore/static/js/2.3101ede5.chunk.js"
   },
   {
-    "revision": "fb9e131d8e642f3597ae",
+    "revision": "5fbdad14d5373972ab42",
     "url": "/bookstore/static/css/main.654fb85e.chunk.css"
   },
   {
-    "revision": "95316da1f12ad2672aa51c7f8ff6b71f",
+    "revision": "79335f1127fba9e891c01a1ddea50d8d",
     "url": "/bookstore/index.html"
   }
 ];
