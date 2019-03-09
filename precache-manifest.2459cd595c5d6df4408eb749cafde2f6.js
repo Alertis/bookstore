1d46866ba82af7d62b3dd15e6f7d9eb9064006e6
Alertis <osmanyavuzdemir96@gmail.com>
2019-03-09 22:46:09 +0300

    Updates

---
+++ precache-manifest.2459cd595c5d6df4408eb749cafde2f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5eccaefd38ad2505c0e",
+    "url": "/bookstore/static/js/runtime~main.c5eccaef.js"
+  },
+  {
+    "revision": "468a4e3c59c2abdcbd7e",
+    "url": "/bookstore/static/js/main.468a4e3c.chunk.js"
+  },
+  {
+    "revision": "3924fa8b4e664f0881aa",
+    "url": "/bookstore/static/js/2.3924fa8b.chunk.js"
+  },
+  {
+    "revision": "468a4e3c59c2abdcbd7e",
+    "url": "/bookstore/static/css/main.654fb85e.chunk.css"
+  },
+  {
+    "revision": "8a1517c5bab9dc17254f549f002f8126",
+    "url": "/bookstore/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/bookstore/precache-manifest.b928d792f4969166126a3e51ef484c21.js"
+  "/bookstore/precache-manifest.2459cd595c5d6df4408eb749cafde2f6.js"
 );
 
 workbox.clientsClaim();
